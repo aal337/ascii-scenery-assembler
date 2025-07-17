82288d38fc410f514effc7e13db8b40bc87b697b
aal337 <86559793+aal337@users.noreply.github.com>
2025-07-17 15:24:54 +0000

    Add project structure and configuration for some things

---
+++ src/asciiasm/main.py
@@ -0,0 +1,11 @@
+"""Main entry point for application."""
+
+from asciiasm import editor, control, files
+
+
+def main():
+    """Run the main application control flow."""
+
+
+if __name__ == "__main__":
+    main()
