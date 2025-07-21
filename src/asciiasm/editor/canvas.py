8fe7eb306fc882d5d68fce2dfab4fbd29f17d4e6
aal337 <abdurahmanalgheb@gmail.com>
2025-07-21 20:22:42 +0000

    Add more stubs to Canvas and Sprite classes

--- src/asciiasm/editor/canvas.py
+++ src/asciiasm/editor/canvas.py
@@ -1,8 +1,21 @@
 """Provide canvas editing and rendering utility."""
 
+from pathlib import Path
+from asciiasm.editor.sprites import Sprite
+
 class Canvas:
     """Canvas on which sprites are placed."""
+
+    sprites: list[Sprite]
 
     def serialise(self) -> str:
         """Return a string representation of the canvas."""
         return ""
+
+    def files(self) -> dict[Path, Sprite]:
+        """Return a list of files to save as work progress."""
+        return {Path(""): Sprite("")}
+
+    def add_sprite(self, sprite: Sprite):
+        """Add a sprite to the canvas."""
+        self.sprites.append(sprite)
--- src/asciiasm/editor/sprites.py
+++ src/asciiasm/editor/sprites.py
@@ -2,8 +2,18 @@
 Filling whitespace recognition and stripping of surrounding whitespace.
 """
 
+import numpy as np
+
 class Sprite:
     """Represent a sprite that can be placed on a canvas."""
 
+    name: str
+    height: int
+    width: int
+
     def __init__(self, text: str):
-        self.text = text   # don't worry I'll add things later
+        pass
+
+    def serialise(self) -> str:
+        """Return a string representation of the sprite."""
+        return ""
