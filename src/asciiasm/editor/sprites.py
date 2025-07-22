3c4a336d3420d3f907e80df90f9ce076676849cf
julianhofbauer6 <julianhofbauer6@gmail.com>
2025-07-22 17:01:14 +0000

    Fix syntax errors

--- src/asciiasm/editor/sprites.py
+++ src/asciiasm/editor/sprites.py
@@ -30,11 +30,11 @@ class Sprite:
         """Strip outer whitespace and replace it with None."""
         def config(*args):
             return {"takes height": args[0], "j start": args[1], "j height": args[2], "sign": args[3]}
-        orientations = {"left": {"takes height" True: "j start": 0, "j height": False, "sign": 1},
+        orientations = {"left": {"takes height": True, "j start": 0, "j height": False, "sign": 1},
         "right": config(True, self.width, False, -1), "top": config(False, 0, True, 1), "bottom": config(False, self.height, True, -1)}
-        for orientation, values in orientations.items():
+        for __, values in orientations.items():
             for i in range(self.height if values["takes height"] else self.width): # self.height, width of current line, width of current line
                 j = values["j start"] # length of current line, 0, self.height
-                while pointer := (self.grid[i][j] if not values["j height"] else self.grid[j][i]) in {" ", None}: # i,j , j,i , j,i
-                    pointer = None # same
+                while (self.grid[i][j] if (condition := not values["j height"]) else self.grid[j][i]) in {" ", None}: # i,j , j,i , j,i
+                    self.grid[i if condition else j][j if condition else i] = None # same
                     j += values["sign"] # -1 for right and bottom
