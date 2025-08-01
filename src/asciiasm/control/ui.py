32f0a2f18e1d524d36d63dad61de8c230b010e15
aal337 <abdurahmanalgheb@gmail.com>
2025-08-01 15:30:31 +0000

    Add basis for screen drawing and other things

--- src/asciiasm/control/ui.py
+++ src/asciiasm/control/ui.py
@@ -1,16 +1,21 @@
 """todo"""
 
-import unicurses
+from unicurses import *
 from .commands import Command
 from ..editor import canvas
 
 class Session:
-    def __init__(self) -> None:
-        # unicurses things todo
+
+    def __init__(self, stdscr) -> None:
+        self.stdscr = stdscr
         self.canvas = canvas.Canvas()
 
     def take_input(self, command: str) -> Command:
         return Command(command)
 
     def display_canvas(self):
+        pass
+
+    def execute(self, command: Command) -> None:
+        pass
         
--- src/asciiasm/main.py
+++ src/asciiasm/main.py
@@ -1,12 +1,20 @@
 """Main entry point for application."""
 
-from asciiasm.editor import canvas, sprites
-from asciiasm.control import commands
-from asciiasm.files import load, save
+from unicurses import *
+from .editor import canvas, sprites
+from .control import commands, ui
+from .files import load, save
 
 
 def main():
     """Run the main application control flow."""
+    main_canvas = canvas.Canvas()
+    stdscr = initscr()
+    # i know that i don't know if this will work
+    session = ui.Session(stdscr)
+    while True:
+        command = session.take_input()
+        session.execute(command)
 
 
 if __name__ == "__main__":
