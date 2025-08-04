3b4a6d7183241ccd09563e9eb6d6eb0f7812cca1
aal337 <86559793+aal337@users.noreply.github.com>
2025-08-04 20:40:35 +0000

    Adapt imports to debugging needs

--- src/asciiasm/main.py
+++ src/asciiasm/main.py
@@ -1,20 +1,23 @@
 """Main entry point for application."""
 
 import sys
-from unicurses import *
-from .editor import canvas, sprites
-from .control import commands, ui
-from .files import load, save
+from unicurses import initscr, endwin, refresh, clear
+from asciiasm.editor import canvas, sprites
+from asciiasm.control import ui
+from asciiasm.files import load, save
 
 
 def main():
     """Run the main application control flow."""
     stdscr = initscr()
     try:
+        clear()
+        refresh()
         session = ui.Session(stdscr)
         while True:
             command = session.take_input()
             session.execute(command)
+            session.display_canvas()
     except KeyboardInterrupt:
         endwin()
         sys.exit(0)
