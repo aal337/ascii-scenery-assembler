b0eb135390217da9ec8882d4a32b7246969bf925
aal337 <86559793+aal337@users.noreply.github.com>
2025-08-04 20:15:28 +0000

    Fully handle commands in ui.py instead of external module

--- src/asciiasm/control/ui.py
+++ src/asciiasm/control/ui.py
@@ -1,26 +1,35 @@
 """todo"""
 
+from pathlib import Path
 from unicurses import move, addstr, refresh, getstr
-from .commands import Command
-from ..editor import canvas
+from ..editor import canvas, sprites
+from ..files import load, save
 
 class Session:
+    sprites: dict[str, sprites.Sprite]
 
     def __init__(self, scr) -> None:
         self.canvas = canvas.Canvas()
         self.scr = scr
 
-    def take_input(self) -> Command:
+    def take_input(self) -> str:
         move(self.scr.getmaxy() - 1, 0)
         addstr("Enter command: ")
         refresh()
         command = getstr().decode("utf-8")
-        return Command(command)
+        return command
 
     def display_canvas(self):
         for line in self.canvas.serialise().split("\n"):
             addstr(line + "\n")
 
-    def execute(self, command: Command) -> None:
-        command.command_func(*command.args)
-        
+    def execute(self, command: str) -> None:
+        command_words = command.split(" ")
+        match " ".join(command_words[:2]):
+            case "place sprite":
+                self.canvas.add_sprite(self.sprites[command_words[2]])
+            case "save canvas":
+                save.save_image(self.canvas, Path(command_words[2]))
+            case "load sprite":
+                sprite = load.load_sprite(Path(command_words[2]))
+                self.sprites[sprite.name] = sprite
