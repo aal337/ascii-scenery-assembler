ec984bb9d959bcc4a679188d74ce513f23c79ccb
aal337 <86559793+aal337@users.noreply.github.com>
2025-08-06 21:47:46 +0000

    Implement canvas serialisation

--- src/asciiasm/editor/canvas.py
+++ src/asciiasm/editor/canvas.py
@@ -10,7 +10,46 @@ class Canvas:
 
     def serialise(self) -> str:
         """Return a string representation of the canvas."""
-        return ""
+        layers: dict[int, list[Sprite]] = {}
+        layer_grids: dict[int, list[list[str | None]]] = {}
+        max_layer = 0
+        width = 0
+        height = 0
+        for sprite in self.sprites:
+            try:
+                # group sprites by layer
+                layers[sprite.layer].append(sprite)
+            except KeyError:
+                layers.update({sprite.layer: [sprite]})
+            # find dimensions of canvas
+            if sprite.layer > max_layer:
+                max_layer = sprite.layer
+            if y := sprite.row + sprite.height > height:
+                height = x
+            if x := sprite.column + sprite.width > width:
+                width = x
+        # create a grid for each layer and fill it
+        for i in range(max_layer + 1):
+            layer_grids.update({i: [[None for __ in range(width)] for __ in range(height)]})
+            for sprite in layers[i]:
+                # dumb approach for now: take each sprite and fill its area in the grid
+                # TODO: check for overlaps
+                for y, row in enumerate(sprite.grid, sprite.row):
+                    for x, char in enumerate(row, sprite.column):
+                        layer_grids[i][y][x] = char
+        # create the string representation by:
+        # 1. iterating through the cells of the final canvas
+        # 2. find the first non-None character throughout all layers
+        result_grid = [[" " for __ in range(width)] for __ in range(height)]
+        for y in range(height):
+            for x in range(width):
+                for layer in range(max_layer, -1, -1):
+                    if char := layer_grids[layer][y][x] is not None:
+                        result_grid[y][x] = char
+                        break
+        # 3. join characters into a string
+        return "\n".join(["".join(row) for row in result_grid])
+
 
     def files(self) -> dict[Path, Sprite]:
         """Return a list of files to save as work progress."""
